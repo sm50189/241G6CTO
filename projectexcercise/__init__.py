f63ec9a31750e740f675cc25833a31d4d74e42c0
VISITTOR <chattarin.rodpon@gmail.com>
2016-10-11 15:01:34 +0700

    1.0

--- projectexcercise/__init__.py
+++ projectexcercise/__init__.py
@@ -8,5 +8,6 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     config.include('.models')
     config.include('.routes')
+    config.add_static_view(name='static', path='projectexcercise:static')
     config.scan()
     return config.make_wsgi_app()
